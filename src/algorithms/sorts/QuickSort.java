aadfd3f9df52414b24ad533497c53ce4d9176ff1
Riley <rbauer1212@gmail.com>
2014-11-15 17:35:00 -0500

    these classes/problems are not finished

---
+++ src/algorithms/sorts/QuickSort.java
@@ -0,0 +1,10 @@
+package algorithms.sorts;
+
+/**
+ * Created by riley on 11/14/14.
+ */
+public class QuickSort<T extends Comparable<T>> {
+    public T[] quickSort(T[] array){
+        return null;
+    }
+}
