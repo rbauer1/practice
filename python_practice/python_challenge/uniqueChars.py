6f143d6fdd031d3f18c077c16086dcfe79e81c2a
Riley Bauer <rbauer1212@gmail.com>
2015-02-03 15:17:01 -0500

    added dir for data for python challenge problems and solved #2

---
+++ python_practice/python_challenge/uniqueChars.py
@@ -0,0 +1,18 @@
+from os import chdir
+from string import ascii_letters as ascii
+
+
+def findUniqueChars():
+    chdir(
+        '/home/riley/workspace/Practice/python_practice/python_challenge/data')
+    f = open('uniqueChars_data')
+    for line in f:
+        for char in line:
+            if char in ascii:
+                print(char, end="", flush=True)
+    print()
+    #below will print all ascii chars in the file, but they are out of order
+    #uniqueChars = set(f.read())
+    #print(''.join([c for c in uniqueChars if c in ascii]))
+
+findUniqueChars()
